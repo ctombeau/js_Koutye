9564e0da3d23faf95dabad6b480de963aa809e83
Tombeau Chrisnor <tombeauc@gmail.com>
2024-03-03 15:17:36 -0500

    Add User

--- src/app/components/addUser/addUser.js
+++ src/app/components/addUser/addUser.js
@@ -1,4 +1,19 @@
-
+/*
 $(document).ready(function(){
     alert("Hello world");
 });
+*/
+/*
+$("form#message").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mess/somefile.php",
+      contentType: "application/json",
+      data: "message_subject="+ message_subject + "&message_text=" + message_text,
+      success: function(){
+        $(".message_field").html('Thanks!');
+      }
+    });
+    return false;
+  });
+  */
